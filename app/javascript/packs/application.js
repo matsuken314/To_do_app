4e149971cc11522b1b1bf2f8097b2066de94ea59
matsuken314 <kenei031455@gmail.com>
2021-09-20 10:00:38 +0900

    Bootstrapの導入

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,7 +7,18 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
+import 'bootstrap';
+import '../stylesheets/application';
+
 
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
+
+// 作成したファイルをwebpackerに読み込ませます。
+import "../stylesheets/application"
+import "../js/hoge.js"
+
+// app/javascript/images/フォルダー内の001.jpeg画像を
+// webpackerに読み込ませます。
+const images = require.context('../images', true)
