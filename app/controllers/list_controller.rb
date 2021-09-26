92df510c21f528209ea4ab316976cc34cb6b12c4
matsuken314 <kenei031455@gmail.com>
2021-09-26 17:11:37 +0900

    削除機能のエラー

--- app/controllers/list_controller.rb
+++ app/controllers/list_controller.rb
@@ -1,5 +1,5 @@
 class ListController < ApplicationController
- before_action :set_list, only: %i(edit update)
+ before_action :set_list, only: %i(edit update destroy)
   def new
     @list = List.new
   end
@@ -24,6 +24,10 @@ class ListController < ApplicationController
     end
   end
 
+  def destroy
+    @list.destroy
+    redirect_to root_path
+  end
   private
     def list_params
       params.require(:list).permit(:title).merge(user: current_user)
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  devise_scope :user do
-    get '/users/sign_out' => 'devise/sessions#destroy'
-  end
+  # devise_scope :user do
+  #   get '/users/sign_out' => 'devise/sessions#destroy'
+  # end
   root 'top#index'
-  resources :list, only: %i(new create edit update)
+  resources :list, only: %i(new create edit update destroy)
+
+  
 end
