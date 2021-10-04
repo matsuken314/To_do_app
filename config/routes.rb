0e64f3b7fe847c7b88a345c9cb0885d388de181b
matsuken314 <kenei031455@gmail.com>
2021-10-04 23:02:55 +0900

    カード作成機能を実装

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,7 @@ Rails.application.routes.draw do
   #   get '/users/sign_out' => 'devise/sessions#destroy'
   # end
   root 'top#index'
-  resources :list, only: [:new, :create, :edit, :update, :destroy]
+  resources :list, only: %i(new create edit update destroy) do
+    resources :card, only: %i(new create)
+  end
 end
